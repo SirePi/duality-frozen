4c6801522bc99f5193381069dba97127f9a3e44b
SirePi <yamailbox@gmail.com>
2014-04-03 07:29:39 +0200

    Working on SkinnedCommandGrid

--- FrozenCore/Widgets/SkinnedCommandGrid.cs
+++ FrozenCore/Widgets/SkinnedCommandGrid.cs
@@ -9,6 +9,7 @@ using FrozenCore.Resources.Widgets;
 using System.Collections.Generic;
 using OpenTK.Input;
 using Duality.Resources;
+using Duality.Components;
 
 namespace FrozenCore.Widgets
 {
@@ -27,6 +28,9 @@ namespace FrozenCore.Widgets
         private Vector2 _gridCellSize;
 
         [NonSerialized]
+        private object _selectedItem;
+
+        [NonSerialized]
         private FormattedText _fText;
 
         [NonSerialized]
@@ -35,11 +39,17 @@ namespace FrozenCore.Widgets
         [NonSerialized]
         private ushort _columns;
 
+        [NonSerialized]
+        private GameObject _highlight;
+
+        [NonSerialized]
+        private SkinnedPanel _highlightPanel;
+
         #endregion NonSerialized fields
 
         private ContentRef<Script> _onLeftClick;
         private ContentRef<Script> _onRightClick;
-        
+
         private ColorRgba _textColor;
         private object _leftClickArgument;
         private object _rightClickArgument;
@@ -57,6 +67,34 @@ namespace FrozenCore.Widgets
 
         private List<object> _items;
         private ContentRef<Font> _textFont;
+
+        public int SelectedIndex
+        {
+            get { return (_selectedItem != null ? _items.IndexOf(_selectedItem) : -1); }
+            set
+            {
+                if (value >= 0 && value < _items.Count)
+                {
+                    SelectedItem = _items[value];
+                }
+            }
+        }
+
+        public object SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                if (_items.Contains(value) && _selectedItem != value)
+                {
+                    _selectedItem = value;
+                }
+                else
+                {
+                    _selectedItem = null;
+                }
+            }
+        }
 
         public List<object> Items
         {
@@ -173,6 +211,10 @@ namespace FrozenCore.Widgets
             _keyRight = Key.Right;
             _keyUp = Key.Up;
             _keyDown = Key.Down;
+
+            _items = new List<object>();
+            _rows = 1;
+            _columns = 1;
         }
 
         public override void MouseDown(OpenTK.Input.MouseButtonEventArgs e)
@@ -193,28 +235,48 @@ namespace FrozenCore.Widgets
 
         protected override void DrawCanvas(Canvas inCanvas)
         {
-            if (_items.Count > 0)
+            if (_items != null && _items.Count > 0)
             {
                 inCanvas.PushState();
                 inCanvas.State.ColorTint = _textColor;
                 inCanvas.State.TransformAngle = GameObj.Transform.Angle;
 
+                UpdateGrid();
+
                 Vector3 origin = _points[5].WorldCoords;
 
-                int row = 0;
-                for (int i = 0; i < _items.Count; i++ )
+                // calculate rows and columns
+                _rows = (ushort)MathF.Ceiling((_points[9].WorldCoords - _points[5].WorldCoords).Length / _gridCellSize.Y);
+                if (_rows == 0)
                 {
-                    ushort itemColumn = (ushort)MathF.Ceiling(i / _rows);
+                    _rows = 1;
+                }
+                _columns = (ushort)MathF.Ceiling(_items.Count / _rows);
+
+                int lastColumn = 0;
+                int itemRow = 0;
+
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    ushort itemColumn = (ushort)MathF.Floor(i / _rows);
+
+                    if (itemColumn != lastColumn)
+                    {
+                        lastColumn = itemColumn;
+                        itemRow = 0;
+                    }
 
                     if (itemColumn >= _leftmostColumn)
                     {
                         _fText.SourceText = _items[i].ToString();
-                        Vector3 topLeft = origin + new Vector3(_gridCellSize.X * row, _gridCellSize.Y * (itemColumn - _leftmostColumn), DELTA_Z);
+                        Vector3 topLeft = origin + new Vector3(_gridCellSize.X * (itemColumn - _leftmostColumn), _gridCellSize.Y * itemRow, DELTA_Z);
 
                         inCanvas.DrawText(_fText, topLeft.X, topLeft.Y, topLeft.Z, null, Alignment.TopLeft);
                     }
+
+                    itemRow++;
                 }
-                
+
                 inCanvas.PopState();
             }
         }
@@ -225,22 +287,35 @@ namespace FrozenCore.Widgets
 
             if (_itemsAccessed)
             {
-                _gridCellSize = Vector2.Zero;
+                UpdateGrid();
+            }
+        }
 
+        protected override void OnInit(Component.InitContext inContext)
+        {
+            base.OnInit(inContext);
+        }
+
+        private void UpdateGrid()
+        {
+            _gridCellSize = Vector2.Zero;
+
+            if (_items != null)
+            {
                 foreach (object o in _items)
                 {
                     if (o != null)
                     {
                         _fText.SourceText = o.ToString();
+                        if (_textFont.Res != null && _fText.Fonts[0] != _textFont)
+                        {
+                            _fText.Fonts[0] = _textFont;
+                        }
 
                         _gridCellSize.X = Math.Max(_gridCellSize.X, _fText.Size.X);
                         _gridCellSize.Y = Math.Max(_gridCellSize.Y, _fText.Size.Y);
                     }
                 }
-
-                // calculate rows and columns
-                _rows = (ushort)MathF.Floor((_points[9].WorldCoords - _points[5].WorldCoords).Length / _gridCellSize.Y);
-                _columns = (ushort)MathF.Ceiling(_items.Count / _rows);
             }
         }
 
@@ -264,6 +339,44 @@ namespace FrozenCore.Widgets
                 {
 
                 }
+            }
+        }
+
+        private void AddHighlight()
+        {
+            _highlight = new GameObject("highlight", this.GameObj);
+
+            Transform t = _highlight.AddComponent<Transform>();
+            t.RelativePos = new Vector3(Rect.W / 2, Rect.H / 2, DELTA_Z / 2);
+            t.RelativeAngle = 0;
+
+            _highlightPanel = new SkinnedPanel();
+            _highlightPanel.VisibilityGroup = this.VisibilityGroup;
+            _highlightPanel.Skin = HighlightSkin;
+            _highlightPanel.Rect = Rect.AlignTopLeft(0, 0, 0, 0);
+            _highlightPanel.OverrideAutomaticZ = true;
+
+            _highlight.AddComponent<SkinnedPanel>(_highlightPanel);
+            Scene.Current.AddObject(_highlight);
+        }
+
+        private void UpdateHighlight()
+        {
+            if (SelectedIndex >= 0)
+            {
+                Rect selectionRect = _fText.TextMetrics.LineBounds[SelectedIndex];
+
+                Vector3 relativePos = _highlight.Transform.RelativePos;
+                relativePos.X = Skin.Res.Border.X;
+                relativePos.Y = Skin.Res.Border.Y + selectionRect.Y;
+
+                Rect highlightRect = _highlightPanel.Rect;
+                highlightRect.H = selectionRect.H;
+                highlightRect.W = Rect.W - Skin.Res.Border.X - Skin.Res.Border.W;
+
+                _highlight.Transform.RelativePos = relativePos;
+                _highlightPanel.Rect = highlightRect;
+                _highlightPanel.VisibleRect = highlightRect;
             }
         }
     }
